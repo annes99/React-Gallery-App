d876c4c5008f2cecfe873d7dbbadd71c067cadfb
Annes Silde <47981031+annes99@users.noreply.github.com>
2019-05-12 22:26:46 +0700

    add error.js

---
+++ src/components/Error.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Error = () => (
+    <li className="not-found">
+        <h3>Error 404</h3>
+        <p>Page not found.</p>
+    </li>
+)
+
+export default Error;
